7d3266bd776e83e91754e6db3b7c25fd0429b855
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-06-25 15:46:46 +0200

    Add first part of ex8

--- src/test/java/java8/ex08/Stream_08_Test.java
+++ src/test/java/java8/ex08/Stream_08_Test.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
@@ -20,121 +23,142 @@ import static org.junit.Assert.*;
  */
 public class Stream_08_Test {
 
-    // Chemin vers un fichier de données des naissances
-    private static final String NAISSANCES_DEPUIS_1900_CSV = "./naissances_depuis_1900.csv";
+	// Chemin vers un fichier de données des naissances
+	private static final String NAISSANCES_DEPUIS_1900_CSV = "/Users/antoinelabeeuw/Documents/workspace-spring-tool-suite-4-4.6.2.RELEASE/java-java8/src/main/resources/naissances_depuis_1900.csv";
 
-    private static final String DATA_DIR = "./pizza-data";
+	private static final String DATA_DIR = "./pizza-data";
 
+	// Structure modélisant les informations d'une ligne du fichier
+	class Naissance {
+		String annee;
+		String jour;
+		Integer nombre;
 
-    // Structure modélisant les informations d'une ligne du fichier
-    class Naissance {
-        String annee;
-        String jour;
-        Integer nombre;
+		public Naissance(String annee, String jour, Integer nombre) {
+			this.annee = annee;
+			this.jour = jour;
+			this.nombre = nombre;
+		}
 
-        public Naissance(String annee, String jour, Integer nombre) {
-            this.annee = annee;
-            this.jour = jour;
-            this.nombre = nombre;
-        }
+		public String getAnnee() {
+			return annee;
+		}
 
-        public String getAnnee() {
-            return annee;
-        }
+		public void setAnnee(String annee) {
+			this.annee = annee;
+		}
 
-        public void setAnnee(String annee) {
-            this.annee = annee;
-        }
+		public String getJour() {
+			return jour;
+		}
 
-        public String getJour() {
-            return jour;
-        }
+		public void setJour(String jour) {
+			this.jour = jour;
+		}
 
-        public void setJour(String jour) {
-            this.jour = jour;
-        }
+		public Integer getNombre() {
+			return nombre;
+		}
 
-        public Integer getNombre() {
-            return nombre;
-        }
+		public void setNombre(Integer nombre) {
+			this.nombre = nombre;
+		}
 
-        public void setNombre(Integer nombre) {
-            this.nombre = nombre;
-        }
-    }
+	}
 
+	private Function<String, Naissance> creaNaissance = (String s) -> {
+		String[] ligneCoupee = s.split(";");
+		Naissance naissance = new Naissance(ligneCoupee[1], ligneCoupee[2], Integer.parseInt(ligneCoupee[3]));
+		return naissance;
+	};
 
-    @Test
-    public void test_group() throws IOException {
+	@Test
+	public void test_group() throws IOException {
 
-        // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
-        // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+		// TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de
+		// lignes du fichier naissances_depuis_1900.csv
+		// Le bloc try(...) permet de fermer (close()) le stream après utilisation
+		try (Stream<String> lines = Files.lines(Paths.get(NAISSANCES_DEPUIS_1900_CSV))) {
+			// TODO construire une MAP (clé = année de naissance, valeur = somme des nombres
+			// de naissance de l'année)
+			// collectors.summingInt
 
-            // TODO construire une MAP (clé = année de naissance, valeur = somme des nombres de naissance de l'année)
-            Map<String, Integer> result = null;
+			// Creation d'une liste de Naissances
+			List<Naissance> listeNaissances = lines.skip(1).map(creaNaissance).collect(Collectors.toList());
+			// passage en map et grouping par annee, somme du nombre de naissance par annee
+			Map<String, Integer> result = listeNaissances.stream()
+					.collect(Collectors.groupingBy(Naissance::getAnnee, summingInt(Naissance::getNombre)));
 
+			assertThat(result.get("2015"), is(8097));
+			assertThat(result.get("1900"), is(5130));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            assertThat(result.get("2015"), is(8097));
-            assertThat(result.get("1900"), is(5130));
-        }
-    }
+	@Test
+	public void test_max() throws IOException {
 
-    @Test
-    public void test_max() throws IOException {
+		// TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de
+		// lignes du fichier naissances_depuis_1900.csv
+		// Le bloc try(...) permet de fermer (close()) le stream après utilisation
+		try (Stream<String> lines = Files.lines(Paths.get(NAISSANCES_DEPUIS_1900_CSV))) {
 
-        // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
-        // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+			// TODO trouver l'année où il va eu le plus de nombre de naissance
+			// Creation d'une liste de Naissances
+			List<Naissance> listeNaissances = lines.skip(1).map(creaNaissance).collect(Collectors.toList());
+			// passage en map et grouping par annee, somme du nombre de naissance par annee
+			
+			Optional<Naissance> result = null;
 
-            // TODO trouver l'année où il va eu le plus de nombre de naissance
-            Optional<Naissance> result = null;
+			assertThat(result.get().getNombre(), is(48));
+			assertThat(result.get().getJour(), is("19640228"));
+			assertThat(result.get().getAnnee(), is("1964"));
+		}
+	}
 
+	@Test
+	public void test_collectingAndThen() throws IOException {
+		// TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de
+		// lignes du fichier naissances_depuis_1900.csv
+		// Le bloc try(...) permet de fermer (close()) le stream après utilisation
+		try (Stream<String> lines = null) {
 
-            assertThat(result.get().getNombre(), is(48));
-            assertThat(result.get().getJour(), is("19640228"));
-            assertThat(result.get().getAnnee(), is("1964"));
-        }
-    }
+			// TODO construire une MAP (clé = année de naissance, valeur = maximum de nombre
+			// de naissances)
+			// TODO utiliser la méthode "collectingAndThen" à la suite d'un "grouping"
+			Map<String, Naissance> result = null;
 
-    @Test
-    public void test_collectingAndThen() throws IOException {
-        // TODO utiliser la méthode java.nio.file.Files.lines pour créer un stream de lignes du fichier naissances_depuis_1900.csv
-        // Le bloc try(...) permet de fermer (close()) le stream après utilisation
-        try (Stream<String> lines = null) {
+			assertThat(result.get("2015").getNombre(), is(38));
+			assertThat(result.get("2015").getJour(), is("20150909"));
+			assertThat(result.get("2015").getAnnee(), is("2015"));
 
-            // TODO construire une MAP (clé = année de naissance, valeur = maximum de nombre de naissances)
-            // TODO utiliser la méthode "collectingAndThen" à la suite d'un "grouping"
-            Map<String, Naissance> result = null;
+			assertThat(result.get("1900").getNombre(), is(31));
+			assertThat(result.get("1900").getJour(), is("19000123"));
+			assertThat(result.get("1900").getAnnee(), is("1900"));
+		}
+	}
 
-            assertThat(result.get("2015").getNombre(), is(38));
-            assertThat(result.get("2015").getJour(), is("20150909"));
-            assertThat(result.get("2015").getAnnee(), is("2015"));
+	// Des données figurent dans le répertoire pizza-data
+	// TODO explorer les fichiers pour voir leur forme
+	// TODO compléter le test
 
-            assertThat(result.get("1900").getNombre(), is(31));
-            assertThat(result.get("1900").getJour(), is("19000123"));
-            assertThat(result.get("1900").getAnnee(), is("1900"));
-        }
-    }
+	@Test
+	public void test_pizzaData() throws IOException {
+		// TODO utiliser la méthode java.nio.file.Files.list pour parcourir un
+		// répertoire
 
-    // Des données figurent dans le répertoire pizza-data
-    // TODO explorer les fichiers pour voir leur forme
-    // TODO compléter le test
+		// TODO trouver la pizza la moins chère
+		String pizzaNamePriceMin = null;
 
-    @Test
-    public void test_pizzaData() throws IOException {
-        // TODO utiliser la méthode java.nio.file.Files.list pour parcourir un répertoire
+		assertThat(pizzaNamePriceMin, is("L'indienne"));
 
-        // TODO trouver la pizza la moins chère
-        String pizzaNamePriceMin = null;
+	}
 
-        assertThat(pizzaNamePriceMin, is("L'indienne"));
-
-    }
-
-    // TODO Optionel
-    // TODO Créer un test qui exporte des données new Data().getPizzas() dans des fichiers
-    // TODO 1 fichier par pizza
-    // TODO le nom du fichier est de la forme ID.txt (ex. 1.txt, 2.txt)
+	// TODO Optionel
+	// TODO Créer un test qui exporte des données new Data().getPizzas() dans des
+	// fichiers
+	// TODO 1 fichier par pizza
+	// TODO le nom du fichier est de la forme ID.txt (ex. 1.txt, 2.txt)
 
 }
